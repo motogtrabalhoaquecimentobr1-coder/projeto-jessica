3502fa7b2b1c8d4b37944d870ba294c7ef0ea1f3
motogtrabalhoaquecimentobr1-coder <motogtrabalhoaquecimentobr1@gmail.com>
2026-03-23 20:26:23 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const WHATSAPP_NUMBER = "5511999999999"; // Substitua pelo seu número real, apenas números (ex: 5511999999999)
+export const WHATSAPP_NUMBER = "5517981611906"; // Substitua pelo seu número real, apenas números (ex: 5511999999999)
